64d6536e737e41cfc8dc81fbd3589e166f474f98
pedro-c-aquino <pedro.aquino@foursys.com.br>
2022-07-08 11:37:21 -0700

    Initial Commit

---
+++ Spotify/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by user208023 on 7/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
