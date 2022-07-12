bf2c1c322cc04f3b0455f71f3e477f2363249b16
pedro-c-aquino <pedro.aquino@foursys.com.br>
2022-07-12 14:59:17 -0700

    Dia 3 - Playlist Spotify

--- Spotify/Controllers/Other/AuthViewController.swift
+++ Spotify/Controllers/Other/AuthViewController.swift
@@ -43,7 +43,15 @@ class AuthViewController: UIViewController, WKNavigationDelegate {
         // Exchange the code for access token
         let component = URLComponents(string: url.absoluteString)
         guard let code = component?.queryItems?.first(where: { $0.name == "code"})?.value else { return }
+        webView.isHidden = true
         print("Code: \(code)")
+        AuthManager.shared.exchangeCodeForToken(code: code) { [weak self] success in
+            DispatchQueue.main.async {
+                self?.navigationController?.popToRootViewController(animated: true)
+                self?.completionHandler?(success)
+            }
+            
+        }
     }
        
 
